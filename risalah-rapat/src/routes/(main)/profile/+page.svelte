aa62341d44599a349fd411e1ece1aa9d4ade3efd
raihan <raihanw0308@gmail.com>
2023-09-08 14:14:41 +0700

    profile screen

--- risalah-rapat/src/routes/(main)/profile/+page.svelte
+++ risalah-rapat/src/routes/(main)/profile/+page.svelte
@@ -1,1 +1,87 @@
-<h1>profile</h1>
+<script lang="ts">
+	// import { Icons } from "$lib/components/docs/icons";
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components//ui/input';
+	import Swal from 'sweetalert2';
+
+	let inputData = {
+		passLama: '',
+		passBaru: '',
+		konfBaru: ''
+	};
+
+	let data = [
+		{
+			passLama: 'ardi12',
+			passBaru: ''
+		}
+	];
+
+	const popupSuccess = () => {
+		Swal.fire({
+			icon: 'success',
+			title: 'Your work has been saved',
+			showConfirmButton: false,
+			timer: 1500
+		});
+	};
+
+	const popupError = () => {
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Something went wrong!'
+		});
+        return;
+	};
+
+	const handlesubmit = () => {
+		if (!inputData.passLama || !inputData.passBaru || !inputData.konfBaru) {
+			popupError();
+		}
+		if (inputData.passLama === data[0].passLama && inputData.passBaru === inputData.konfBaru) {
+			popupSuccess();
+            console.log(inputData);
+		}
+	};
+</script>
+
+<Card.Root>
+	<Card.Header class="space-y-1">
+		<Card.Title class="text-2xl">Profile User</Card.Title>
+	</Card.Header>
+	<Card.Content class="grid gap-4">
+		<div class="grid gap-2">
+			<Label for="passwordLama">Password Lama</Label>
+			<Input
+				id="passwordLama"
+				type="password"
+				placeholder="masukkan password lama"
+				bind:value={inputData.passLama}
+			/>
+		</div>
+		<div class="grid gap-2">
+			<Label for="passwordBaru">Password Baru</Label>
+			<Input
+				id="passwordBaru"
+				type="password"
+				placeholder="masukkan password baru"
+				bind:value={inputData.passBaru}
+			/>
+		</div>
+		<div class="grid gap-2">
+			<Label for="konfirmasiPasswordBaru">Konfirmasi Password Baru</Label>
+			<Input
+				id="konfirmasiPasswordBaru"
+				type="password"
+				placeholder="konfirmasi password baru"
+				bind:value={inputData.konfBaru}
+			/>
+		</div>
+	</Card.Content>
+	<Card.Footer>
+		<Button class="w-full" on:click={handlesubmit}>Simpan</Button>
+	</Card.Footer>
+</Card.Root>
--- risalah-rapat/src/routes/+layout.svelte
+++ risalah-rapat/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.postcss';
+	import "../app.postcss"
 	import HeaderNav from './(main)/HeaderNav.svelte';
 	let isOpen = false;
 </script>
@@ -40,63 +40,23 @@
 			</li>
 
 			<!-- Text links (hidden on small screens) -->
-			<button
-				type="button"
-				class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-				aria-controls="dropdown-example"
-				data-collapse-toggle="dropdown-example"
-			>
-				<svg
-					class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 18 21"
-				>
-					<path
-						d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
-					/>
-				</svg>
-				<span class="flex-1 ml-3 text-left whitespace-nowrap">E-commerce</span>
-				<svg
-					class="w-3 h-3"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 10 6"
-				>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="m1 1 4 4 4-4"
-					/>
-				</svg>
-			</button>
-			<ul id="dropdown-example" class="hidden py-2 space-y-2">
-				<li>
-					<a
-						href="#"
-						class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-						>Products</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-						>Billing</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-						>Invoice</a
-					>
-				</li>
-			</ul>
+			<li class="mb-2 hidden sm:block relative">
+				<a href="/rapat" class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium"
+				   on:click={() => isOpen = !isOpen}
+				>Rapat</a>
+			
+				<!-- Dropdown menu -->
+				{#if isOpen}
+				<ul class="absolute left-0 mt-2 bg-gray-800 text-white p-2 space-y-1">
+					<li>
+						<a href="rapat/buat-rapat" class="hover:text-gray-300">Buat Rapat</a>
+					</li>
+					<li>
+						<a href="rapat/daftar-rapat" class="hover:text-gray-300">Daftar Rapat</a>
+					</li>
+				</ul>
+				{/if}
+			</li>
 			<!-- Add similar blocks for other links -->
 
 			<!-- Icons (hidden on larger screens) -->
@@ -123,33 +83,6 @@
 			<li class="mb-2 hidden sm:block">
 				<a href="/master" class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium"
 					>Master</a
-				>
-			</li>
-			<!-- Icons (hidden on larger screens) -->
-			<li class="mb-2 sm:hidden">
-				<!-- Insert icon code here -->
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-presentation"
-					><path d="M2 3h20" /><path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3" /><path
-						d="m7 21 5-5 5 5"
-					/></svg
-				>
-			</li>
-
-			<!-- Text links (hidden on small screens) -->
-			<li class="mb-2 hidden sm:block">
-				<a
-					href="/profile"
-					class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium">Profile</a
 				>
 			</li>
 			<!-- Icons (hidden on larger screens) -->
