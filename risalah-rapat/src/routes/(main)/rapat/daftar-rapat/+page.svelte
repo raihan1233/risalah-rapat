139adf3f81d31bd1bdf0f0034ac61459314cbfab
raihan <raihanw0308@gmail.com>
2023-09-08 16:14:26 +0700

    update again

--- risalah-rapat/src/routes/(main)/rapat/daftar-rapat/+page.svelte
+++ risalah-rapat/src/routes/(main)/rapat/daftar-rapat/+page.svelte
@@ -1,1 +1,111 @@
-<h1>daftar rapat</h1>
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<form>
+    <h1 class="font-bold fs-10">Daftar Rapat</h1>
+	<div class="space-y-12">
+		<div class="border-b border-gray-900/10 pb-12">
+			<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+				<div class="col-span-full">
+					<label for="waktu-mulai" class="block text-sm font-medium leading-6 text-gray-900">Waktu Mulai</label>
+					<div class="mt-2">
+						<input
+							id="waktu-mulai"
+                            type="datetime-local"
+							name="waktu mulai"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+                <div class="col-span-full">
+					<label for="waktu-selesai" class="block text-sm font-medium leading-6 text-gray-900">Waktu Selesai</label>
+					<div class="mt-2">
+						<input
+							id="waktu-selesai"
+                            type="datetime-local"
+							name="waktu selesai"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="col-span-full">
+					<label for="status" class="block text-sm font-medium leading-6 text-gray-900">Status</label>
+					<div class="mt-2">
+						<select
+							id="status"
+							name="status"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						>
+                        <option selected disabled>pilih opsi</option>
+                        <option value="belum approve">belum approve</option>
+                        <option value="terkirim">terkirim</option>
+                        <option value="publish">publish</option>
+                    </select>
+					</div>
+				</div>
+
+                <div class="col-span-full">
+					<label for="tipe" class="block text-sm font-medium leading-6 text-gray-900">Tipe</label>
+					<div class="mt-2">
+						<select
+							id="tipe"
+							name="tipe"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						>
+                        <option selected disabled>pilih opsi</option>
+                        <option value="checker">checker</option>
+                        <option value="approver">approver</option>
+                        <option value="tembusan">tembusan</option>
+                    </select>
+					</div>
+				</div>
+
+                <div class="col-span-full">
+					<label for="perihal" class="block text-sm font-medium leading-6 text-gray-900">Perihal</label>
+					<div class="mt-2">
+						<input
+							id="perihal"
+                            type="text"
+							name="perihal"
+                            placeholder="masukkan perihal"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+                <div class="col-span-full">
+					<label for="tempat" class="block text-sm font-medium leading-6 text-gray-900">Tempat</label>
+					<div class="mt-2">
+						<input
+							id="tempat"
+                            type="text"
+							name="tempat"
+                            placeholder="masukkan tempat"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="mt-6 flex items-center gap-x-6">
+		<button
+			type="submit"
+			class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Cari</button
+		>
+	</div>
+</form>
--- risalah-rapat/src/routes/+layout.svelte
+++ risalah-rapat/src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script>
 	import "../app.postcss"
 	import HeaderNav from './(main)/HeaderNav.svelte';
-	let isOpen = false;
+	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+	
+	let isOpenRapat = false;
+	let isOpenMaster = false;
+
+	function toggleDropdown(dropdownName) {
+        if (dropdownName === 'rapat') {
+            isOpenRapat = !isOpenRapat;
+            isOpenMaster = false;
+        } else if (dropdownName === 'master') {
+            isOpenMaster = !isOpenMaster;
+            isOpenRapat = false;
+        }
+    }
 </script>
 
 <div class="flex h-screen">
@@ -12,7 +25,7 @@
 
 		<ul class="mt-12">
 			<!-- Text links (hidden on small screens) -->
-			<li class="mb-2 hidden sm:block">
+			<li class="mb-3 hidden sm:block">
 				<a href="/home" class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium"
 					>Home</a
 				>
@@ -20,9 +33,9 @@
 			<!-- Add similar blocks for other links -->
 
 			<!-- Icons (hidden on larger screens) -->
-			<li class="mb-2 sm:hidden">
-				<!-- Insert icon code here -->
-				<svg
+			<!-- <li class="mb-5 sm:hidden">
+				<a href="/home">
+					<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
 					height="24"
@@ -37,31 +50,31 @@
 						points="9 22 9 12 15 12 15 22"
 					/></svg
 				>
-			</li>
+				</a>
+			</li> -->
 
 			<!-- Text links (hidden on small screens) -->
-			<li class="mb-2 hidden sm:block relative">
-				<a href="/rapat" class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium"
-				   on:click={() => isOpen = !isOpen}
-				>Rapat</a>
-			
-				<!-- Dropdown menu -->
-				{#if isOpen}
-				<ul class="absolute left-0 mt-2 bg-gray-800 text-white p-2 space-y-1">
-					<li>
-						<a href="rapat/buat-rapat" class="hover:text-gray-300">Buat Rapat</a>
-					</li>
-					<li>
-						<a href="rapat/daftar-rapat" class="hover:text-gray-300">Daftar Rapat</a>
-					</li>
-				</ul>
-				{/if}
+			<li class="mb-3 hidden sm:block relative">
+				<div class="group relative">
+					<button
+						class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium focus:outline-none flex align-middle"
+						on:click={() => toggleDropdown('rapat')}
+					>
+						<span class="me-3">Rapat</span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+					</button>
+					{#if isOpenRapat}
+						<div class="absolute z-10 mt-2 bg-gray-800 text-white border rounded-lg shadow-lg top-0 right-5">
+							<!-- Dropdown menu items -->
+							<a href="/rapat/buat-rapat" class="block px-4 py-2 hover:bg-gray-700">Buat Rapat</a>
+							<a href="/rapat/daftar-rapat" class="block px-4 py-2 hover:bg-gray-700">Daftar Rapat</a>
+						</div>
+					{/if}
+				</div>
 			</li>
 			<!-- Add similar blocks for other links -->
 
 			<!-- Icons (hidden on larger screens) -->
-			<li class="mb-2 sm:hidden">
-				<!-- Insert icon code here -->
+			<!-- <li class="mb-5 sm:hidden">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="24"
@@ -77,33 +90,68 @@
 						d="m7 21 5-5 5 5"
 					/></svg
 				>
-			</li>
+			</li> -->
 
 			<!-- Text links (hidden on small screens) -->
-			<li class="mb-2 hidden sm:block">
-				<a href="/master" class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium"
-					>Master</a
-				>
+			<li class="mb-3 hidden sm:block relative">
+				<div class="group relative">
+					<button
+						class="text-gray-300 hover:text-white block px-3 py-2 text-sm font-medium focus:outline-none flex align-middle"
+						on:click={() => toggleDropdown('master')}
+					>
+						<span class="me-3">Master</span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+					</button>
+					{#if isOpenMaster}
+						<div class="absolute z-10 mt-2 bg-gray-800 text-white border rounded-lg shadow-lg top-0 right-5">
+							<!-- Dropdown menu items -->
+							<a href="/master/tempat" class="block px-4 py-2 hover:bg-gray-700">Master Tempat</a>
+							<a href="/master/template" class="block px-4 py-2 hover:bg-gray-700">Master Template</a>
+							<a href="/master/user" class="block px-4 py-2 hover:bg-gray-700">Master User</a>
+						</div>
+					{/if}
+				</div>
 			</li>
 			<!-- Icons (hidden on larger screens) -->
-			<li class="mb-2 sm:hidden">
-				<!-- Insert icon code here -->
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-presentation"
-					><path d="M2 3h20" /><path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3" /><path
-						d="m7 21 5-5 5 5"
-					/></svg
-				>
-			</li>
+			<!-- <li class="mb-5 sm:hidden">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-template"><rect width="18" height="7" x="3" y="3" rx="1"/><rect width="9" height="7" x="3" y="14" rx="1"/><rect width="5" height="7" x="16" y="14" rx="1"/></svg>
+			</li> -->
+			<ul>
+				<!-- Icons for small screens -->
+				<li class="mb-5 sm:hidden">
+					<a href="#" on:click={() => toggleDropdown('rapat')}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home">
+							<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+							<polyline points="9 22 9 12 15 12 15 22" />
+						</svg>
+					</a>
+				</li>
+	
+				<li class="mb-5 sm:hidden">
+					<a href="#" on:click={() => toggleDropdown('master')}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-presentation">
+							<path d="M2 3h20" />
+							<path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3" />
+							<path d="m7 21 5-5 5 5" />
+						</svg>
+					</a>
+				</li>
+	
+				<!-- ... (your existing code) ... -->
+			</ul>
+	
+			<!-- Dropdown containers for small screens -->
+			<div class={isOpenRapat ? "sm:hidden" : "hidden"}>
+				<!-- Dropdown menu items -->
+				<a href="/rapat/buat-rapat" class="block px-4 py-2 hover:bg-gray-700">Buat Rapat</a>
+				<a href="/rapat/daftar-rapat" class="block px-4 py-2 hover:bg-gray-700">Daftar Rapat</a>
+			</div>
+	
+			<div class={isOpenMaster ? "sm:hidden" : "hidden"}>
+				<!-- Dropdown menu items -->
+				<a href="/master/tempat" class="block px-4 py-2 hover:bg-gray-700">Master Tempat</a>
+				<a href="/master/template" class="block px-4 py-2 hover:bg-gray-700">Master Template</a>
+				<a href="/master/user" class="block px-4 py-2 hover:bg-gray-700">Master User</a>
+			</div>
 		</ul>
 	</div>
 	<div class="flex-1">
