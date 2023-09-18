1b798ee38ca6f8f652545fadab65ce8ec03b69cc
raihan <raihanw0308@gmail.com>
2023-09-18 10:01:55 +0700

    fix add master template

--- risalah-rapat/src/routes/(main)/master/template/add-template.svelte
+++ risalah-rapat/src/routes/(main)/master/template/add-template.svelte
@@ -5,9 +5,24 @@
   import { Label } from "$lib/components/ui/label";
   import { Separator } from "$lib/components/ui/separator";
   import * as RadioGroup from "$lib/components/ui/radio-group";
+  import { createEventDispatcher } from "svelte";
   import Swal from 'sweetalert2';
 
+  export let data;
+  
+  const dispatch = createEventDispatcher();
+
   const saveData = () => {
+    const newNo = data.length + 1;
+    const newData = {
+      no: newNo, // Generate a new number based on the current data length
+      nama_template: inputData.template, // Example value, replace with actual input data
+      file: inputData.unggahTemplate,
+      status: 'Aktif', // Example value, replace with actual input data
+    };
+
+    // Emit an event with the new data
+    dispatch('dataSaved', newData);
 		Swal.fire({
 			icon: 'success',
 			title: 'Data berhasil ditambahkan',
@@ -15,6 +30,11 @@
 			timer: 1500
 		});
 	};
+
+  let inputData = {
+    template: "",
+    unggahTemplate: ""
+  }
 </script>
 
 <Dialog.Root>
@@ -29,11 +49,11 @@
     <div class="py-4 space-y-4">
       <div class="space-y-4">
         <Label for="template">Template</Label>
-        <Input id="template" placeholder="Masukkan nama template" />
+        <Input id="template" placeholder="Masukkan nama template" bind:value={inputData.template} />
       </div>
       <div class="grid w-full max-w-sm items-center gap-x-1.5 space-y-4">
           <Label for="unggah-template">Unggah Template</Label>
-          <Input id="unggah-template" type="file" class="cursor-pointer" />
+          <Input id="unggah-template" type="file" class="cursor-pointer"  bind:value={inputData.unggahTemplate}/>
       </div>
       <div class="space-y-4">
         <Label>Status</Label>
--- risalah-rapat/src/routes/(main)/master/template/data-table.svelte
+++ risalah-rapat/src/routes/(main)/master/template/data-table.svelte
@@ -1,92 +1,110 @@
 <script lang="ts">
-  import { createTable, Render, Subscribe, createRender } from "svelte-headless-table";
-  import { readable } from "svelte/store";
-  // import * as Table from "$lib/components/ui/table";
-  import { addPagination, addSortBy, addTableFilter } from "svelte-headless-table/plugins";
-  import { Button } from "$lib/components/ui/button";
-  import { ArrowUpDown } from "lucide-svelte";
-  import { Input } from "$lib/components/ui/input";
-  import EditTemplate from "./edit-template.svelte";
-  import AddTemplate from './add-template.svelte';
-  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, TableSearch } from 'flowbite-svelte';
+	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
+	import { readable } from 'svelte/store';
+	// import * as Table from "$lib/components/ui/table";
+	import { addPagination, addSortBy, addTableFilter } from 'svelte-headless-table/plugins';
+	import { Button } from '$lib/components/ui/button';
+	import { ArrowUpDown } from 'lucide-svelte';
+	import { Input } from '$lib/components/ui/input';
+	import EditTemplate from './edit-template.svelte';
+	import AddTemplate from './add-template.svelte';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		TableSearch
+	} from 'flowbite-svelte';
 
-  const data = [
-    {
+	let data = [
+		{
 			no: 1,
 			nama_template: 'Invoice',
 			file: 'invoice_template.docx',
-			status: 'Aktif',
+			status: 'Aktif'
 		},
 		{
 			no: 2,
 			nama_template: 'Report',
 			file: 'report_template.docx',
-			status: 'Aktif',
+			status: 'Aktif'
 		},
 		{
 			no: 3,
 			nama_template: 'Letter',
 			file: 'letter_template.docx',
-			status: 'Tidak Aktif',
+			status: 'Tidak Aktif'
 		},
 		{
 			no: 4,
 			nama_template: 'Resume',
 			file: 'resume_template.docx',
-			status: 'Aktif',
+			status: 'Aktif'
 		},
 		{
 			no: 5,
 			nama_template: 'Proposal',
 			file: 'proposal_template.docx',
-			status: 'Aktif',
+			status: 'Aktif'
 		},
 		{
 			no: 6,
 			nama_template: 'Agreement',
 			file: 'agreement_template.docx',
-			status: 'Tidak Aktif',
+			status: 'Tidak Aktif'
 		},
 		{
 			no: 7,
 			nama_template: 'Presentation',
 			file: 'presentation_template.pptx',
-			status: 'Aktif',
+			status: 'Aktif'
 		},
 		{
 			no: 8,
 			nama_template: 'Form',
 			file: 'form_template.pdf',
-			status: 'Aktif',
+			status: 'Aktif'
 		}
-  ];
+	];
 
-    let searchTerm = '';
-  	$: filteredItems = data.filter((item) => {
-        const lowerSearchTerm = searchTerm.toLowerCase();
-        return Object.values(item).some((value) =>
-            typeof value === 'string' &&
-            value.toLowerCase().includes(lowerSearchTerm)
-        );
-    });
+	const handleDataSaved = (event) => {
+    // Append the new data to the existing data array
+    data = [...data, event.detail];
+  };
+
+	let searchTerm = '';
+	$: filteredItems = data.filter((item) => {
+		const lowerSearchTerm = searchTerm.toLowerCase();
+		return Object.values(item).some(
+			(value) => typeof value === 'string' && value.toLowerCase().includes(lowerSearchTerm)
+		);
+	});
 </script>
 
-
 <div class="space-y-4 overflow-x-auto">
-	<AddTemplate />
+	<AddTemplate on:dataSaved={handleDataSaved} {data} />
 
 	<div class="sm:flex sm:justify-end">
-		<TableSearch placeholder="Cari di sini" hoverable={true} bind:inputValue={searchTerm} classInput="mb-4 px-8 py-2 rounded-md border border-gray-300 w-full sm:max-w-xs" classSvgDiv="p-2" divClass="shadow-none relative"></TableSearch>
+		<TableSearch
+			placeholder="Cari di sini"
+			hoverable={true}
+			bind:inputValue={searchTerm}
+			classInput="mb-4 px-8 py-2 rounded-md border border-gray-300 w-full sm:max-w-xs"
+			classSvgDiv="p-2"
+			divClass="shadow-none relative"
+		/>
 	</div>
 
 	<Table shadow>
 		<TableHead class="text-sm">
 			<!-- <tr> -->
-				<TableHeadCell class="!p-4">No</TableHeadCell>
-				<TableHeadCell class="!p-4">Nama Template</TableHeadCell>
-        <TableHeadCell class="!p-4">File</TableHeadCell>
-				<TableHeadCell class="!p-4">Status</TableHeadCell>
-				<TableHeadCell class="!p-4">Aksi</TableHeadCell>
+			<TableHeadCell class="!p-4">No</TableHeadCell>
+			<TableHeadCell class="!p-4">Nama Template</TableHeadCell>
+			<TableHeadCell class="!p-4">File</TableHeadCell>
+			<TableHeadCell class="!p-4">Status</TableHeadCell>
+			<TableHeadCell class="!p-4">Aksi</TableHeadCell>
 			<!-- </tr> -->
 		</TableHead>
 		<TableBody class="divide-y">
@@ -94,11 +112,11 @@
 				<TableBodyRow>
 					<TableBodyCell class="!p-4">{row.no}</TableBodyCell>
 					<TableBodyCell class="!p-4">{row.nama_template}</TableBodyCell>
-          <TableBodyCell class="!p-4">{row.file}</TableBodyCell>
+					<TableBodyCell class="!p-4"><a href={data[0].file} class="underline" download>{row.file}</a></TableBodyCell>
 					<TableBodyCell class="!p-4">{row.status}</TableBodyCell>
 					<TableBodyCell class="!p-4">
 						<!-- {#each row.Aksi as aksiOption} -->
-            <EditTemplate />
+						<EditTemplate />
 						<!-- {/each} -->
 					</TableBodyCell>
 				</TableBodyRow>
