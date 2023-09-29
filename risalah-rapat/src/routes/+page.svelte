0860deb7f37ed38e35b69025b8201bcb0dad99ec
raihan <raihanw0308@gmail.com>
2023-09-29 10:26:50 +0700

    fetch api on page login

--- risalah-rapat/src/routes/+page.svelte
+++ risalah-rapat/src/routes/+page.svelte
@@ -5,14 +5,25 @@
 	import { Input } from '$lib/components/ui/input';
 	import '../app.postcss';
 	import Swal from 'sweetalert2';
-  	import { Eye, EyeOff } from "lucide-svelte";
+	import { Eye, EyeOff } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 
-	const data = [
-		{
-			username: 'admin',
-			password: 'admin'
+	let data = [];
+	const fetchUsers = async () => {
+		try {
+			const response = await fetch('http://localhost:3000/users'); // Replace with your API endpoint
+			if (response.ok) {
+				data = await response.json();
+				console.log(data);
+			} else {
+				console.error('Failed to fetch data from the API');
+			}
+		} catch (error) {
+			console.error('Error fetching data:', error);
 		}
-	];
+	};
+
+	onMount(fetchUsers);
 
 	const popupSuccess = () => {
 		Swal.fire(`selamat datang ${inputData.username} !`, 'tunggu beberapa saat', 'success');
@@ -32,23 +43,42 @@
 		password: ''
 	};
 
-	const handleSubmit = () => {
-		if (!inputData.username || inputData.password) {
-			popupError();
-		}
-		if (inputData.username === data[0].username && inputData.password === data[0].password) {
-			popupSuccess();
-			setTimeout(() => {
-				window.location.href = '/home';
-			}, 1500);
-		}
-	};
+	const handleSubmit = async () => {
+        if (!inputData.username || !inputData.password) {
+            popupError();
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(inputData),
+            });
+
+            if (response.ok) {
+                // Login successful
+                popupSuccess();
+                setTimeout(() => {
+                    window.location.href = '/home';
+                }, 1500);
+            } else {
+                // Login failed
+                popupError();
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            popupError();
+        }
+    }
 
 	let showPassword = false;
 
-  function togglePasswordVisibility() {
-    showPassword = !showPassword;
-  }
+	function togglePasswordVisibility() {
+		showPassword = !showPassword;
+	}
 </script>
 
 <div class="min-h-screen background">
@@ -60,21 +90,34 @@
 			<Card.Content class="grid gap-4">
 				<div class="grid gap-2">
 					<Label for="username">Username</Label>
-					<Input id="username" type="text" placeholder="e.g: username12" bind:value={inputData.username} />
+					<Input
+						id="username"
+						type="text"
+						placeholder="e.g: username12"
+						bind:value={inputData.username}
+					/>
 				</div>
 				<div class="grid gap-2">
 					<Label for="password">Password</Label>
 					<div class="relative w-full">
-						<Input id="password" type={showPassword ? 'text' : 'password'} placeholder="masukkan password" bind:value={inputData.password} />
-						<div class="absolute inset-y-0 right-0 flex items-center p-3 focus:outline-none" on:click={togglePasswordVisibility}>
+						<Input
+							id="password"
+							type={showPassword ? 'text' : 'password'}
+							placeholder="masukkan password"
+							bind:value={inputData.password}
+						/>
+						<div
+							class="absolute inset-y-0 right-0 flex items-center p-3 focus:outline-none"
+							on:click={togglePasswordVisibility}
+						>
 							{#if showPassword}
 								<!-- Eye icon open -->
-									<Eye class="h-4 w-4 cursor-pointer" />
+								<Eye class="h-4 w-4 cursor-pointer" />
 							{:else}
-							<!-- Eye icon closed -->
-									<EyeOff class="h-4 w-4 cursor-pointer" />
+								<!-- Eye icon closed -->
+								<EyeOff class="h-4 w-4 cursor-pointer" />
 							{/if}
-					</div>	
+						</div>
 					</div>
 				</div>
 			</Card.Content>
